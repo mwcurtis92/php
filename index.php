187e3beb0f41079f3c1e2d5d81a98164d3f889d6
mwcurtis92 <logic_engineer_curtis@yahoo.com>
2014-04-28 17:21:24 -0600

    testing

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>mwbodily's webpage</title>
-
+   <link href = "homeStyle.css" type = "text/css" rel = "stylesheet" />
 </head>
 <body>
 <p>Hello World!</p>
