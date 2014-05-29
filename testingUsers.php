87f4e4a82105b1c043ded43feb0be56a3c7ca8b6
mwcurtis92 <logic_engineer_curtis@yahoo.com>
2014-05-29 12:34:02 -0600

    project 2 test

--- testingUsers.php
+++ testingUsers.php
@@ -1,14 +1,39 @@
 <?php
-   try
+   
+   	$dbHost = "";
+   $dbPort = "";
+   $dbUser = "";
+   $dbPassword = "";
+
+   $dbName = "testdb";
+
+	$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+   if($openShiftVar === null || $openShiftVar == "")
    {
       $user = "php";
-      $password = "Jordin656";
-      $db = new PDO("mysql:host=localhost;dbname=students", $user, $password);
+         $password = "Jordin656";
+         $host = getenv ('OpenShift')
+      
+         $db = new PDO("mysql:host=localhost;dbname=students", $user, $password);
+      }
+      catch (PDOException $ex)
+      {
+         echo "Error!: " . $ex->getMessage();
+         die();
+      }
    }
-   catch (PDOException $ex)
-   {
-      echo "Error!: " . $ex->getMessage();
-      die();
+   else{
+      $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+      $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      }
+
+?>
+
+
    }
 ?>
 <html>
