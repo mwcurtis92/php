73939af2fe02adae951709fcbc37108ba9dee599
mwcurtis92 <logic_engineer_curtis@yahoo.com>
2014-06-02 15:13:12 -0600

    project code, for realsies

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/xampp/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<html>
+<head>
+   <title>mwbodily's home page</title>
+</head>
+
+<body>
+   <a href = "assignments.html">Assignments</a>
+</body>
+
+
+</html>
