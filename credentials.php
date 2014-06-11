c0cc42895398a499c07ebdb17c6a3c00b051c5e0
mwcurtis92 <logic_engineer_curtis@yahoo.com>
2014-06-11 10:34:28 -0600

    project update

--- credentials.php
+++ credentials.php
@@ -34,7 +34,8 @@ echo "here 1";
       $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
       $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
       $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+      //$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+      $dbPassword = "Jordin656";
       $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
       }
       catch (PDOException $ex)
