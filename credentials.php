9a03424cbb8644b39d42750496181a1c5bd7219f
mwcurtis92 <logic_engineer_curtis@yahoo.com>
2014-06-11 10:31:55 -0600

    project update

--- credentials.php
+++ credentials.php
@@ -7,28 +7,42 @@
    $db = "";
    $dbName = "students";
 	$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-
+echo "here 1";
    if($openShiftVar === null || $openShiftVar == "")
    {
       try
       {
-         $user = "php";
+         echo "here2";
+        $user = "php";
          $password = "Jordin656";
       
          $db = new PDO("mysql:host=localhost;dbname=students", $user, $password);
       }
       catch (PDOException $ex)
       {
+        
          echo "Error! Please contact your system administrator. ";
          die();
       }
    }
    else
    {
+      echo "here3";
+      try
+      {
+         echo "here4";
       $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
       $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
       $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
       $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
       $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-   }  
+      }
+      catch (PDOException $ex)
+      {
+         echo "Error! Please contact your system administrator. ";
+         var_dump($ex);
+         die();
+      }
+   }     
+   echo "here5";
 ?>
